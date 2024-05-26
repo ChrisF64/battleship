b7c812013ec8e7a05178889b9c93778986670989
Chris Farrell <CT_Farrell@outlook.com>
2024-05-26 11:10:26 +0000

    Created Playboard class

--- run.py
+++ run.py
@@ -12,3 +12,80 @@ def select_random_up_down():
     return random.choice(up_down)
 def select_random_left_right():
     return random.choice(left_right)
+
+# Playing field class
+
+class Playfield :
+
+    #Creates an instance of a playing field
+    def __init__(self):
+        self.empty = """
+         ____ ____ ____ ____ ____ ____ ____ ____ ____ ____
+        |    |    |    |    |    |    |    |    |    |    |
+        | A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 | A8 | A9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | B0 | B1 | B2 | B3 | B4 | B5 | B6 | B7 | B8 | B9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | C0 | C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 | D9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | E0 | E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8 | E9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | F0 | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | G0 | G1 | G2 | G3 | G4 | G5 | G6 | G7 | G8 | G9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | H0 | H1 | H2 | H3 | H4 | H5 | H6 | H7 | H8 | H9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | I0 | I1 | I2 | I3 | I4 | I5 | I6 | I7 | I8 | I9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        |    |    |    |    |    |    |    |    |    |    |
+        | J0 | J1 | J2 | J3 | J4 | J5 | J6 | J7 | J8 | J9 |
+        |____|____|____|____|____|____|____|____|____|____|
+        """
+
+        self.rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        self.columns = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.fields = [self.rows[i] + column for i in range(10) for column in self.columns]
+
+    # Populates the board and returns a list of occupied spaces as well as a list containing all placed ships.
+    def populate(self, ship_list):
+        available_fields = self.fields.copy()
+        occupied_fields = []
+        for ship in ship_list:
+            while ship.is_occupied(available_fields):
+                ship.place(self.fields)
+            occupied_fields += ship.fields
+            ship.remove(available_fields)
+        return occupied_fields, ship_list
+
+    # Updates board appearance after each player move
+    def update(self, hit_list, miss_list):
+        update = self.empty
+        for field in hit_list:
+            update = update.replace( " " + field + " ", "0000")
+        for field in miss_list:
+            update = update.replace(" " + field + " ", "~~~~")
+        return update
+        
+    # Updates board appearance to show where ships are located
+    def solution(self, occupied_fields):
+        solution = self.empty
+        for field in self.fields:
+            if field in occupied_fields:
+                solution = solution.replace(" " + field + " ", "0000")
+            else:
+                solution = solution.replace(" " + field + " ", "~~~~")
+        return solution
+
+
+    
