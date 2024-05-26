c5566f8ce87ae1a2351de300336669e7350a9207
Chris Farrell <CT_Farrell@outlook.com>
2024-05-26 12:18:29 +0000

    Created ship class and function for placing ships on play board

--- run.py
+++ run.py
@@ -13,11 +13,11 @@ def select_random_up_down():
 def select_random_left_right():
     return random.choice(left_right)
 
-# Playing field class
+# Playing board class
 
 class Playfield :
 
-    #Creates an instance of a playing field
+    #Creates an instance of a playing board
     def __init__(self):
         self.empty = """
          ____ ____ ____ ____ ____ ____ ____ ____ ____ ____
@@ -72,9 +72,9 @@ class Playfield :
     def update(self, hit_list, miss_list):
         update = self.empty
         for field in hit_list:
-            update = update.replace( " " + field + " ", "0000")
+            update = update.replace( " " + field + " ", "@@@@")
         for field in miss_list:
-            update = update.replace(" " + field + " ", "~~~~")
+            update = update.replace(" " + field + " ", "----")
         return update
         
     # Updates board appearance to show where ships are located
@@ -82,10 +82,84 @@ class Playfield :
         solution = self.empty
         for field in self.fields:
             if field in occupied_fields:
-                solution = solution.replace(" " + field + " ", "0000")
+                solution = solution.replace(" " + field + " ", "@@@@")
             else:
-                solution = solution.replace(" " + field + " ", "~~~~")
+                solution = solution.replace(" " + field + " ", "----")
         return solution
 
 
+# Ship class
+class Ship:
     
+    # Creates an instance of a ship in the form of a list containing the pre-defined number of fields
+    def __init__(self, size):
+        self.size = size
+        self.fields = []
+        for i in range(size):
+            self.fields.append("")
+    
+    # Randomly places ship on the playing board
+    def place(self, playing_fields):
+        self.playing_fields = playing_fields
+        self.fields[0] = select_random_field(playing_fields)
+        if self.size > 1:     
+            index_counter = playing_fields.index(self.fields[0])
+            dist = select_random_distribution()
+            if dist == "horizontal":
+                if str(self.size-2) in self.fields[0] or str(self.size-3) in self.fields[0] or str(self.size-4) in self.fields[0]:
+                    dir = "right"
+                elif str(11-self.size) in self.fields[0] or str(12-self.size) in self.fields[0] or str(13-self.size) in self.fields[0]:
+                    dir = "left"
+                else:
+                    dir = select_random_left_right()
+                for i in range(self.size-1):
+                    if dir == "right":
+                        index_counter += 1
+                        self.fields[i+1] = playing_fields[index_counter]
+                    else:
+                        index_counter -=1
+                        self.fields[i+1] = playing_fields[index_counter]
+            else:
+                if index_counter < (self.size-1)*10:
+                    dir = "down"
+                elif index_counter > (len(playing_fields)-1) - (self.size-1)*10:
+                    dir = "up"
+                else:
+                    dir = select_random_up_down()
+                for i in range(self.size-1):
+                    if dir == "down":
+                        index_counter += 10
+                        self.fields[i+1] = playing_fields[index_counter]
+                    else:
+                        index_counter -=10
+                        self.fields[i+1] = playing_fields[index_counter]
+        return self.fields
+    
+    # Remove the fields occupied by the ship and surrounded no longer selectable fields from the available fields
+    def remove(self, from_fields):
+        fields_to_be_removed = []
+        available_fields = Playfield().fields
+        for i in range(self.size):
+            idx = available_fields.index(self.fields[i])
+            if self.fields[i] == "A0":
+                fields_to_be_removed += [available_fields[idx], available_fields[idx+1], available_fields[idx+10], available_fields[idx+11]]
+            elif self.fields[i] == "J9":
+                fields_to_be_removed += [available_fields[idx], available_fields[idx-1], available_fields[idx-10], available_fields[idx-11]]
+            elif "A" in self.fields[i]:
+                fields_to_be_removed += [available_fields[idx], available_fields[idx-1], available_fields[idx+1], available_fields[idx+9], available_fields[idx+10], available_fields[idx+11]]
+            elif "J" in self.fields[i]:
+                fields_to_be_removed += [available_fields[idx], available_fields[idx-1], available_fields[idx-9], available_fields[idx-10], available_fields[idx-11], available_fields[idx+1]]
+            elif "0" in self.fields[i]:
+                fields_to_be_removed += [available_fields[idx], available_fields[idx-9], available_fields[idx-10], available_fields[idx+1], available_fields[idx+10], available_fields[idx+11]]
+            elif "9" in self.fields[i]:
+                fields_to_be_removed += [available_fields[idx], available_fields[idx-1], available_fields[idx-10], available_fields[idx-11], available_fields[idx+9], available_fields[idx+10]]
+            else:
+                fields_to_be_removed += [available_fields[idx], available_fields[idx-1], available_fields[idx-9], available_fields[idx-10], available_fields[idx-11], available_fields[idx+1], available_fields[idx+9], available_fields[idx+10], available_fields[idx+11]]
+        for field in fields_to_be_removed:
+            if field in from_fields:
+                from_fields.pop(from_fields.index(field))
+        return from_fields
+
+    # Check if the fields selected for the ship are already occupied
+    def is_occupied(self, available_fields):
+        return any(field not in available_fields for field in self.fields)
