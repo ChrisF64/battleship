b31b6c31e0d41a79651c693219739c15ae8d60ef
Chris Farrell <CT_Farrell@outlook.com>
2024-05-26 10:30:01 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Functions to randomly populate playing field
+
+import random
+distribution = ["horizontal", "vertical"]
+up_down = ["up", "down"]
+left_right = ["left", "right"]
+def select_random_field(selection):
+    return random.choice(selection)
+def select_random_distribution():
+    return random.choice(distribution)
+def select_random_up_down():
+    return random.choice(up_down)
+def select_random_left_right():
+    return random.choice(left_right)
